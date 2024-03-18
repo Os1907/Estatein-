16b7f2ee48bd880e1653676adba31eaa9497eb57
os1907 <osamatrezey@gmail.com>
2024-03-18 07:19:27 +0200

    edited taps

--- src/components/Properties/Discover/CarusolDiscover/CarusolDiscover.jsx
+++ src/components/Properties/Discover/CarusolDiscover/CarusolDiscover.jsx
@@ -120,7 +120,7 @@ export default function CarusolDiscover() {
                   <div className="titles-Card">
                   <div className="my-3 flex lg:justify-start justify-center w-full flex-wrap gap-y-1   ">
                       <div className="flex border border-[#262626] rounded-3xl items-center px-5 sm:px-3 mx-1 py-1 bg-[#1a1a1a]">
-                          <p className="xl:text-sm text-[7px] sm:text-[9px] sm:ml-2 ml-1 text-white line-clamp-1"> {item.BuildDisc}-Bedroom </p>
+                          <p className="m  text-sm sm:ml-2 ml-1 text-white line-clamp-1"> {item.BuildDisc}-Bedroom </p>
                       </div>
                   </div>
                      <h3 className="text-white lg:text-lg text-base text-left font-semibold">
@@ -133,7 +133,7 @@ export default function CarusolDiscover() {
                       <div className="w-full flex justify-between">
                           <div>
                               <p className="text-[#9999] text-[12px]   sm:text-sm lg:text-base ">price</p>
-                              <p className="text-white text-sm lg:text-base font-medium">{item.price}</p>
+                              <p className="text-white text-base lg:text-base font-medium">{item.price}</p>
                           </div>
                           <Link to='/product' className=" py-3 px-4 text-white  my-1 bg-[#703BF7]  text-[10px]  lg:rounded-xl rounded-lg sm:text-sm lg:text-base ">
                           View Property Details
